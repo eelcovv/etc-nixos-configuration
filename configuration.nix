e6b3b5ac5ed40662fe7bf152b8ed1fe6567144db
Eelco <eelcovv@gmail.com>
2025-01-03 13:19:19 +0100

    added flakes

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,8 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   services.blueman.enable = true;
-	
+
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 }
