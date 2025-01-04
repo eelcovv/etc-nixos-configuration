4fcd1f736aff3796e7a9c71d48b02fedfd2d7a3a
Eelco <eelcovv@gmail.com>
2025-01-04 23:30:11 +0100

    turned off blueman

--- configuration.nix
+++ configuration.nix
@@ -132,7 +132,7 @@
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
+  # services.blueman.enable = true;
 
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
